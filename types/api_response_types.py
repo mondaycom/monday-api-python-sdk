c3fe37d71bbbb5bab2ff4162dc4247ecfbc8b152
Michael Imas <michaelim@monday.com>
2024-08-22 16:43:56 +0300

    removed board id

--- types/api_response_types.py
+++ types/api_response_types.py
@@ -100,7 +100,7 @@ example usage:
 
 import dacite
 
-response = fetch_board_items(6430059868, '2024-06-23', '2024-07-01')
+response = fetch_board_items(BOARD_ID, START_DATE, END_DATE)
 serialized_response = dacite.from_dict(data_class=MondayApiResponse, data=res) 
 first_item_name = serialized_response.data.boards[0].items_page.items[0].name
 
